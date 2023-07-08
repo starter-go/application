3ca198c23dfa4fe85ab4e13f8215ddc037050fe6
MEZ <mez@bitwormhole.com>
2023-07-08 18:06:09 +0800

    实现了加载系统中的环境变量

--- boot/env_loader.go
+++ boot/env_loader.go
@@ -1,9 +1,33 @@
 package boot
 
+import (
+	"os"
+	"strings"
+)
+
 type envLoader struct {
 	b *Bootstrap
 }
 
 func (inst *envLoader) Load() error {
+	dst := inst.b.collections.Environment
+	src1 := inst.loadFromSystem()
+	src2 := dst.Export(nil)
+	dst.Import(src1)
+	dst.Import(src2)
 	return nil
+}
+
+func (inst *envLoader) loadFromSystem() map[string]string {
+	dst := make(map[string]string)
+	src := os.Environ()
+	for _, item := range src {
+		i := strings.IndexRune(item, '=')
+		if i >= 0 {
+			key := strings.TrimSpace(item[0:i])
+			val := strings.TrimSpace(item[i+1:])
+			dst[key] = val
+		}
+	}
+	return dst
 }
