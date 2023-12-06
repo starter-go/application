b20293c2e75851b1bd0f41167f5bd280ee399229
MEZ <mez@bitwormhole.com>
2023-12-06 21:32:49 +0800

    up to v0.9.19

--- module.go
+++ module.go
@@ -6,8 +6,8 @@ import (
 
 const (
 	theModuleName     = "github.com/starter-go/application"
-	theModuleVersion  = "v0.9.18"
-	theModuleRevision = 2
+	theModuleVersion  = "v0.9.19"
+	theModuleRevision = 3
 )
 
 ////////////////////////////////////////////////////////////////////////////////
